43884396cd4f6ff8c8e0a1caa90a5a25d2d9d33d
Manuel Cherep <manuelcherep@gmail.com>
2025-02-01 20:00:33 -0500

    Allow dropping params and delays in the API call

--- nudging/api.py
+++ nudging/api.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 import litellm
+import time
 
 def create_api_call(
         model,
@@ -28,16 +29,19 @@ def create_api_call(
         tool_choice,
         temperature,
         max_tokens,
-        additional_drop_params
+        additional_drop_params,
+        delay
 ):
     """Factory for creating the api_call function."""
-    return lambda messages, tools: litellm.completion(
-        model=model,
-        messages=messages,
-        tools=tools,
-        parallel_tool_calls=parallel_tool_calls,
-        tool_choice=tool_choice,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        additional_drop_params=list(additional_drop_params)
-    )
+    return lambda messages, tools: (
+        time.sleep(delay),  # Delay before each call
+        litellm.completion(
+            model=model,
+            messages=messages,
+            tools=tools,
+            parallel_tool_calls=parallel_tool_calls,
+            tool_choice=tool_choice,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            additional_drop_params=list(additional_drop_params)
+        ))[1]
